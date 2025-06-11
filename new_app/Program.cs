acbd01290c4fb80a67b4c5100aac5c7e8d4e9a2c
Ostap Semenchuk <72123138+semenchukostap@users.noreply.github.com>
2025-06-11 15:50:49 +0200

    QA: Update Program.cs with CoreWCF configuration

--- new_app/Program.cs
+++ new_app/Program.cs
@@ -3,47 +3,20 @@ using CoreWCF.Configuration;
 using CoreWCF.Description;
 using SOAPWebServices.Core.Contracts;
 using SOAPWebServices.Core.Services;
-using SOAPWebServices.Core.Infrastructure;
 
 var builder = WebApplication.CreateBuilder(args);
-
-// Create and initialize the ApplicationEvents instance
-ApplicationEvents applicationEvents = new ApplicationEvents();
 
 // Add services to the container
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
-// Configure CORS if needed
-builder.Services.AddCors(options =>
-{
-    options.AddDefaultPolicy(builder =>
-    {
-        builder.AllowAnyOrigin()
-               .AllowAnyMethod()
-               .AllowAnyHeader();
-    });
-});
-
 // Configure CoreWCF Services
 builder.Services.AddServiceModelServices();
 builder.Services.AddServiceModelMetadata();
 builder.Services.AddSingleton<IServiceBehavior, UseRequestHeadersForMetadataAddressBehavior>();
 
-// Configure Kestrel to listen on port 8080 (same as the legacy app)
-builder.WebHost.ConfigureKestrel(options =>
-{
-    options.ListenAnyIP(8080);
-});
-
-// Make the ApplicationEvents instance available through DI
-builder.Services.AddSingleton(applicationEvents);
-
 var app = builder.Build();
-
-// Call OnApplicationStarting after the app is built but before it starts
-applicationEvents.OnApplicationStarting();
 
 // Configure the HTTP request pipeline
 if (app.Environment.IsDevelopment())
@@ -55,20 +28,7 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 app.UseRouting();
-app.UseCors();
 app.UseAuthorization();
-
-// Register application lifecycle events middleware
-app.Use(async (context, next) => {
-    try {
-        await next(context);
-    }
-    catch (Exception ex) {
-        applicationEvents.OnError(ex);
-        throw;
-    }
-});
-
 app.MapControllers();
 
 // Configure ServiceModel
@@ -76,31 +36,12 @@ app.UseServiceModel(serviceBuilder =>
 {
     serviceBuilder.AddService<GetDataService>();
     serviceBuilder.AddServiceEndpoint<GetDataService, IGetDataService>(
-        new BasicHttpBinding(),
+        new BasicHttpBinding(), 
         "/GetDataService.svc");
 
     var serviceMetadataBehavior = app.Services.GetRequiredService<ServiceMetadataBehavior>();
     serviceMetadataBehavior.HttpGetEnabled = true;
-    serviceMetadataBehavior.HttpGetUrl = new Uri("http://localhost:8080/GetDataService");
+    serviceMetadataBehavior.HttpGetUrl = new Uri("http://localhost:5000/GetDataService");
 });
-
-// Register application lifecycle events with the host lifetime
-var lifetime = app.Services.GetRequiredService<IHostApplicationLifetime>();
-lifetime.ApplicationStarted.Register(() => {
-    applicationEvents.OnApplicationStarted();
-});
-
-lifetime.ApplicationStopping.Register(() => {
-    applicationEvents.OnApplicationStopping();
-});
-
-lifetime.ApplicationStopped.Register(() => {
-    applicationEvents.OnApplicationStopped();
-});
-
-// Register application shutdown event for non-graceful shutdowns
-AppDomain.CurrentDomain.ProcessExit += (sender, e) => {
-    applicationEvents.OnApplicationStopping();
-};
 
 app.Run();

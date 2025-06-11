9d359cccd01a14ce6f05b235239721c42981e3ca
Ostap Semenchuk <72123138+semenchukostap@users.noreply.github.com>
2025-06-11 22:08:39 +0200

    QA: Update GetDataService implementation with modern C# syntax

--- new_app/SOAPWebServicesCore/Services/GetDataService.cs
+++ new_app/SOAPWebServicesCore/Services/GetDataService.cs
@@ -6,29 +6,16 @@ namespace SOAPWebServicesCore.Services
 {
     public class GetDataService : IGetDataService
     {
-        /// <summary>
-        /// Returns a simple Hello World message
-        /// </summary>
-        /// <returns>A greeting message</returns>
         public string HelloWorld()
         {
             return "Hello World";
         }
 
-        /// <summary>
-        /// Returns a personalized greeting message
-        /// </summary>
-        /// <param name="name">Name of the person to greet</param>
-        /// <returns>A personalized greeting message</returns>
         public string GetData(string name)
         {
             return $"Hello {name}, this is a simple SOAP web service response.";
         }
 
-        /// <summary>
-        /// Creates and returns a sample DataSet
-        /// </summary>
-        /// <returns>A DataSet containing sample data</returns>
         public DataSet GetDataSet()
         {
             var ds = new DataSet("SampleDataSet");
@@ -44,11 +31,6 @@ namespace SOAPWebServicesCore.Services
             return ds;
         }
 
-        /// <summary>
-        /// Returns a report based on the input parameters
-        /// </summary>
-        /// <param name="reportInput">Input parameters for the report</param>
-        /// <returns>A DataSet containing the report data</returns>
         public DataSet GetReport(ref ReportInput reportInput)
         {
             return GetDataSet();

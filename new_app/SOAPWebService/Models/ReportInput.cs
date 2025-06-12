d0098ae5a7ec9784ad57f4684438f9c6755803f3
Ostap Semenchuk <72123138+semenchukostap@users.noreply.github.com>
2025-06-12 10:41:59 +0200

    QA: Update ReportInput model for proper SOAP serialization and .NET 8 compatibility

--- new_app/SOAPWebService/Models/ReportInput.cs
+++ new_app/SOAPWebService/Models/ReportInput.cs
@@ -6,13 +6,13 @@ namespace SOAPWebService.Models
     /// Data transfer object for report input parameters.
     /// Migrated from legacy VB.NET cReportInput class to modern C# with DataContract serialization.
     /// </summary>
-    [DataContract]
+    [DataContract(Name = "ReportInput", Namespace = "http://soapwebservice.com/models")]
     public class ReportInput
     {
         /// <summary>
         /// Gets or sets the name of the report to generate.
         /// </summary>
-        [DataMember]
+        [DataMember(Name = "ReportName", Order = 1, IsRequired = true)]
         public string ReportName { get; set; } = string.Empty;
     }
 }

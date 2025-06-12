e06a40bc4123a8e70f8c469a2ee6eb9aa407e096
Ostap Semenchuk <72123138+semenchukostap@users.noreply.github.com>
2025-06-12 15:01:04 +0200

    QA: Update ReportInput model with proper SOAP serialization attributes
    - Added DataContract attribute with correct namespace (http://tempuri.org/)
    - Added DataMember attributes for proper SOAP serialization
    - Updated to use modern C# property syntax with null safety
    - Added comprehensive XML documentation
    - Ensured compatibility with System.ServiceModel.Primitives package

--- new_app/Models/ReportInput.cs
+++ new_app/Models/ReportInput.cs
@@ -1,10 +1,24 @@
 using System.Runtime.Serialization;
 
-namespace SOAPWebServicesCore.Models;
-
-[DataContract(Namespace = "http://tempuri.org/")]
-public class ReportInput
+namespace SOAPWebServicesCore.Models
 {
-    [DataMember]
-    public string ReportName { get; set; } = string.Empty;
+    /// <summary>
+    /// Represents the input parameters for generating reports in the SOAP web service.
+    /// This class is designed to be compatible with legacy VB.NET cReportInput structure
+    /// and provides proper SOAP serialization support.
+    /// </summary>
+    [DataContract(Namespace = "http://tempuri.org/")]
+    public class ReportInput
+    {
+        /// <summary>
+        /// Gets or sets the name of the report to be generated.
+        /// This property specifies which report template or type should be used
+        /// for report generation.
+        /// </summary>
+        /// <value>
+        /// A string representing the report name. Default value is an empty string.
+        /// </value>
+        [DataMember]
+        public string ReportName { get; set; } = string.Empty;
+    }
 }

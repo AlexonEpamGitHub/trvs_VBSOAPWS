cf32d6e13a8a82de349a871306998fc0d784098d
Ostap Semenchuk <72123138+semenchukostap@users.noreply.github.com>
2025-06-13 11:58:25 +0200

    QA: Updated IDataService.cs with improved SOAP contract definition

--- new_app/SOAPWebServicesCore/Services/IDataService.cs
+++ new_app/SOAPWebServicesCore/Services/IDataService.cs
@@ -1,22 +1,24 @@
 using SOAPWebServicesCore.Models;
 using System.Data;
 using System.ServiceModel;
+using System.ServiceModel.Web;
 
 namespace SOAPWebServicesCore.Services
 {
     [ServiceContract(Namespace = "http://tempuri.org/")]
+    [XmlSerializerFormat(SupportFaults = true)]
     public interface IDataService
     {
-        [OperationContract]
+        [OperationContract(Action = "http://tempuri.org/HelloWorld")]
         string HelloWorld();
         
-        [OperationContract]
+        [OperationContract(Action = "http://tempuri.org/GetData")]
         string GetData(string name);
         
-        [OperationContract]
+        [OperationContract(Action = "http://tempuri.org/GetDataSet")]
         DataSet GetDataSet();
         
-        [OperationContract]
+        [OperationContract(Action = "http://tempuri.org/GetReport")]
         DataSet GetReport(ReportInput reportInput);
     }
 }

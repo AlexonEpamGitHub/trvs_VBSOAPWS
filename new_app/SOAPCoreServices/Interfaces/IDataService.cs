a46f4b312d3d42b3449331047395ab2ed98dde23
Ostap Semenchuk <72123138+semenchukostap@users.noreply.github.com>
2025-06-13 11:11:02 +0200

    QA: Update IDataService with async patterns and XML documentation

--- new_app/SOAPCoreServices/Interfaces/IDataService.cs
+++ new_app/SOAPCoreServices/Interfaces/IDataService.cs
@@ -8,16 +8,30 @@ namespace SOAPCoreServices.Interfaces
     [ServiceContract(Namespace = "http://tempuri.org/")]
     public interface IDataService
     {
+        /// <summary>
+        /// Basic Hello World operation
+        /// </summary>
         [OperationContract]
-        string HelloWorld();
+        Task<string> HelloWorldAsync();
 
+        /// <summary>
+        /// Returns a greeting message with the provided name
+        /// </summary>
+        /// <param name="name">Name to include in greeting</param>
         [OperationContract]
-        string GetData(string name);
+        Task<string> GetDataAsync(string name);
 
+        /// <summary>
+        /// Returns a sample dataset
+        /// </summary>
         [OperationContract]
-        DataSet GetDataSet();
+        Task<DataSet> GetDataSetAsync();
 
+        /// <summary>
+        /// Returns a report based on the input parameters
+        /// </summary>
+        /// <param name="reportInput">Report input parameters</param>
         [OperationContract]
-        DataSet GetReport(Models.ReportInput reportInput);
+        Task<DataSet> GetReportAsync(Models.ReportInput reportInput);
     }
 }

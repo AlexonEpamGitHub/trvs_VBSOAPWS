9fa22cc529daba2ec0c60d0c226decb0276f92d6
Ostap Semenchuk <72123138+semenchukostap@users.noreply.github.com>
2025-06-13 11:58:44 +0200

    QA: Updated ReportInput.cs with proper serialization attributes

--- new_app/SOAPWebServicesCore/Models/ReportInput.cs
+++ new_app/SOAPWebServicesCore/Models/ReportInput.cs
@@ -1,7 +1,14 @@
+using System.Runtime.Serialization;
+using System.Xml.Serialization;
+
 namespace SOAPWebServicesCore.Models
 {
+    [DataContract(Namespace = "http://tempuri.org/")]
+    [XmlRoot("cReportInput", Namespace = "http://tempuri.org/")]
     public class ReportInput
     {
+        [DataMember]
+        [XmlElement("ReportName")]
         public string? ReportName { get; set; }
     }
 }

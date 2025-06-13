44e944818cfcbe8079916e001b569dec57d919b4
Ostap Semenchuk <72123138+semenchukostap@users.noreply.github.com>
2025-06-13 12:05:18 +0200

    QA: Add DataContract attribute to DataService class for better SOAP compatibility
    1. Added System.Runtime.Serialization namespace import
    2. Added DataContract attribute to the service implementation class
    3. Maintained existing ServiceBehavior attribute

--- new_app/SOAPWebServicesCore/Services/DataService.cs
+++ new_app/SOAPWebServicesCore/Services/DataService.cs
@@ -1,10 +1,27 @@
 using SOAPWebServicesCore.Models;
 using System;
 using System.Data;
+using System.Runtime.Serialization;
 using System.ServiceModel;
 
 namespace SOAPWebServicesCore.Services
 {
+    [ServiceContract(Namespace = "http://tempuri.org/")]
+    public interface IDataService
+    {
+        [OperationContract]
+        string HelloWorld();
+
+        [OperationContract]
+        string GetData(string name);
+
+        [OperationContract]
+        DataSet GetDataSet();
+
+        [OperationContract]
+        DataSet GetReport(ReportInput reportInput);
+    }
+
     [ServiceBehavior(Namespace = "http://tempuri.org/")]
     public class DataService : IDataService
     {

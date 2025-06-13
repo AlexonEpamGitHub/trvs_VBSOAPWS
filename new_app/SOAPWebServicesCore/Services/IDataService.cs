eeec337ead591e60c1a98ea0469c417ad3f9a835
Ostap Semenchuk <72123138+semenchukostap@users.noreply.github.com>
2025-06-13 12:09:45 +0200

    QA: Update IDataService interface with improved SOAP contract definition

--- new_app/SOAPWebServicesCore/Services/IDataService.cs
+++ new_app/SOAPWebServicesCore/Services/IDataService.cs
@@ -2,6 +2,7 @@ using SOAPWebServicesCore.Models;
 using System.Data;
 using System.ServiceModel;
 using System.ServiceModel.Web;
+using System.Runtime.Serialization;
 
 namespace SOAPWebServicesCore.Services
 {
@@ -20,5 +21,33 @@ namespace SOAPWebServicesCore.Services
         
         [OperationContract(Action = "http://tempuri.org/GetReport")]
         DataSet GetReport(ReportInput reportInput);
+    }
+
+    [MessageContract(IsWrapped = true, WrapperName = "ReportRequest", WrapperNamespace = "http://tempuri.org/")]
+    public class ReportRequest
+    {
+        [MessageBodyMember(Namespace = "http://tempuri.org/")]
+        public ReportInput ReportInput { get; set; }
+    }
+
+    [MessageContract(IsWrapped = true, WrapperName = "ReportResponse", WrapperNamespace = "http://tempuri.org/")]
+    public class ReportResponse
+    {
+        [MessageBodyMember(Namespace = "http://tempuri.org/")]
+        public DataSet ReportResult { get; set; }
+    }
+
+    [MessageContract(IsWrapped = true, WrapperName = "GetDataRequest", WrapperNamespace = "http://tempuri.org/")]
+    public class GetDataRequest
+    {
+        [MessageBodyMember(Namespace = "http://tempuri.org/")]
+        public string Name { get; set; }
+    }
+
+    [MessageContract(IsWrapped = true, WrapperName = "GetDataResponse", WrapperNamespace = "http://tempuri.org/")]
+    public class GetDataResponse
+    {
+        [MessageBodyMember(Namespace = "http://tempuri.org/")]
+        public string Result { get; set; }
     }
 }

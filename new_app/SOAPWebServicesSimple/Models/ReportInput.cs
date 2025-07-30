9d60a91d5a9fb3f4cb95a45aa380eb6cac2dc431
Ostap Semenchuk <72123138+semenchukostap@users.noreply.github.com>
2025-07-30 09:28:29 +0200

    Add ReportInput class in C# to replace legacy cReportInput.vb

---
+++ new_app/SOAPWebServicesSimple/Models/ReportInput.cs
@@ -0,0 +1,12 @@
+namespace SOAPWebServicesSimple.Models;
+
+public class ReportInput
+{
+    private string _reportName;
+
+    public string ReportName
+    {
+        get => _reportName;
+        set => _reportName = value;
+    }
+}
